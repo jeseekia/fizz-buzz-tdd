6f7cd76dfc3114e0da6671fe8758d046578ceaf4
Jeseekia Vaughn <jeseekia@gmail.com>
2017-05-01 11:32:25 -0400

    Class returns 2 properly

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,5 +1,6 @@
 class FizzBuzz
   def self.convert(input_numbers)
+    return 2 if input_numbers == 2
     1
   end
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -4,4 +4,8 @@ describe FizzBuzz do
   it "returns 1 for input of 1" do
     expect(FizzBuzz.convert(1)).to eq(1)
   end
+
+  it "returns 2 for input of 2" do
+    expect(FizzBuzz.convert(2)).to eq(2)
+  end
 end
